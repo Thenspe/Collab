d68417f982f75327cc0167b55ba1273ac7d5cf6e
S VanDam <stephen.vandam@flemingcollege.ca>
2023-05-11 14:10:09 -0400

    playing with exports

--- Del1/GEO_split.py
+++ Del1/GEO_split.py
@@ -1,27 +1,10 @@
-# Stephen's space for messing around with Python
-# WAT
-
-# Some values are blank, some have both | and ;
-
-# There are two values per pipe - a type and a depth. We only need the depth.
-
-# So, when possible, split by pipe, then by semi-colon, then keep the 2nd values.
-# From those values, check if they are in ft or m.
-# Strip the string characters (' ft' or ' m') and convert to number.
-# If it was in feet, convert the number to m. Else, leave it alone.
-
-# Put them back into WAT, delimited by semicolon.
+# This script creates the Borehole_Results table
+# and splits the field GEO into it
 
 # example_data = ""
 example_data = "BRWN ;SAND ;CLAY ;FILL ;0 m;2.43 m|BRWN ;CLAY ;SILT ; DRY ;2.43 m;3.65 m|BRWN ;SAND ;STNS ;WBRG ;3.65 m;4.57 m|"
 # example_data = "40 ft"
 # example_data = "6 m"
-
-# def ft2m(conv):
-#         val = float(conv.group(5))
-#         if conv.group(6) == " ft": 
-#             val=float(conv.group(5))*0.3048 
-#         return str(val)
 
 import arcpy
 
--- Del1/sv_python.py
+++ Del1/sv_python.py
@@ -1,71 +1,45 @@
 # Stephen's space for messing around with Python
-# WAT
-
-# Some values are blank, some have both | and ;
-
-# There are two values per pipe - a type and a depth. We only need the depth.
-
-# So, when possible, split by pipe, then by semi-colon, then keep the 2nd values.
-# From those values, check if they are in ft or m.
-# Strip the string characters (' ft' or ' m') and convert to number.
-# If it was in feet, convert the number to m. Else, leave it alone.
-
-# Put them back into WAT, delimited by semicolon.
-
-# example_data = ""
-example_data = "BRWN ;SAND ;CLAY ;FILL ;0 m;2.43 m|BRWN ;CLAY ;SILT ; DRY ;2.43 m;3.65 m|BRWN ;SAND ;STNS ;WBRG ;3.65 m;4.57 m|"
-# example_data = "40 ft"
-# example_data = "6 m"
-
-# def ft2m(conv):
-#         val = float(conv.group(5))
-#         if conv.group(6) == " ft": 
-#             val=float(conv.group(5))*0.3048 
-#         return str(val)
 
 import arcpy
 
 workspace="D:\\FlemSem3\\Collab\\CollabProj\\CollabProj.gdb"
 arcpy.env.workspace = workspace
 
-bhResultsTable = 'Borehole_Results'
+bufferedTable = 'Wells_In_Buffer'
+typeTable = 'Well_Type'
 # check if the table already exists, remove it if it does.
-if arcpy.Exists(bhResultsTable):
-    print('Table exists, deleting...')
-    arcpy.Delete_management(bhResultsTable)
+if arcpy.Exists(typeTable):
+    print(typeTable + ' table exists, deleting...')
+    arcpy.Delete_management(typeTable)
     print('Table deleted.')
 print("Creating table.")
 # create the table
-arcpy.management.CreateTable(workspace,bhResultsTable)
+arcpy.conversion.ExportTable(in_table=bufferedTable,out_table=typeTable,use_field_alias_as_name=True,field_mapping=[
+    'WELL_ID',
+    'WELL_COMPLETED_DATE',
+    'RECEIVED_DATE',
+    'TAG',
+    'SWL',
+    'FINAL_STATUS_DESCR',
+    'USE1',
+    'USE2',
+    'UTMZONE',
+    'EAST83',
+    'NORTH83',
+    'PT',
+    'DEPTH_M'
+])
 # set the fields for the new table
-insertFields = ['Well_ID','Colour','Mat1','Mat2','Mat3','Top','Bot']
+# insertFields = ['Well_ID','Colour','Mat1','Mat2','Mat3','Top','Bot']
 # add fields to the table
-arcpy.management.AddFields(bhResultsTable,
-    [[insertFields[0],'LONG',None,15],
-     [insertFields[1],'TEXT','Colour',15],
-     [insertFields[2],'TEXT','Material 1',15],
-     [insertFields[3],'TEXT','Material 2',15],
-     [insertFields[4],'TEXT','Material 3',15],
-     [insertFields[5],'TEXT','Top of Layer',15],
-     [insertFields[6],'TEXT','Bottom of Layer',15]])
+# arcpy.management.AddFields(bhResultsTable,
+#     [[insertFields[0],'LONG',None,15],
+#      [insertFields[1],'TEXT','Colour',15],
+#      [insertFields[2],'TEXT','Material 1',15],
+#      [insertFields[3],'TEXT','Material 2',15],
+#      [insertFields[4],'TEXT','Material 3',15],
+#      [insertFields[5],'TEXT','Top of Layer',15],
+#      [insertFields[6],'TEXT','Bottom of Layer',15]])
 
-# set the relevant fields from the table with the data
-searchFields = ['Well_ID','GEO']
-# create the insert cursor, for populating the new table
-addStuff = arcpy.da.InsertCursor('Borehole_Results', insertFields)
-# create the search cursor, for pulling data from the original table
-pull = arcpy.da.SearchCursor('Wells_In_Buffer', searchFields)
-for row in pull:                # for each row in the original table...
-    # print(pull)
-    pipes = pull[1].split("|")  # separate by the pipes.
-    # print(pipes)
-    for x in pipes:             # for each set of pipes...
-        semicolons = x.split(";")   # separate by the semicolons.
-        # print(pull[0])
-        # print(len(semicolons))
-        if len(semicolons) > 1:     # only add lines that have values in them
-            addStuff.insertRow([pull[0],semicolons[0],semicolons[1],semicolons[2],semicolons[3],semicolons[4],semicolons[5]])
-            # print(semicolons)
-del pull        # close the cursors to prevent errors
-del addStuff
-print('Process complete.')  # Good job, script!
+
+print('Process complete.')

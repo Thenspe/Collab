efad14af89161a938238c4a4a731f2b3e5be7de5
Thenspe <91274102+Thenspe@users.noreply.github.com>
2023-05-26 14:57:16 -0400

    filter geojson by decade

--- Del2/javascript/svCOGmap.js
+++ Del2/javascript/svCOGmap.js
@@ -8,8 +8,8 @@ const Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/re
 });
 //Map declaration
 const map = L.map('map', {
-    center: [44.5, -78],
-    zoom: 8,
+    center: [44.3, -77.8],    //44.5, -78
+    zoom: 12,
     layers: [osm, Esri_WorldImagery]            
 });
 // Add baselayer info as array for layer control, control added lower to put it
@@ -36,20 +36,53 @@ var geocoder = L.Control.geocoder({
 //Add layer control button to switch between imagery and openstreetmap
 const baseControl = L.control.layers(baseLayers,null,{position:'topleft'}).addTo(map);
 
+/////////////////////////////////////////////////////////////////////////////////////////
+// This section attempts to add a control to filter by decade
+
+var layerInfo = {
+    twenties: {source:"192", color: '#FE1A1A'},
+    thirties: {source:"193", color: '#FEAB1A'},
+    fourties: {source:"194", color: '#FEFE1A'},
+    fifties: {source:"195", color: '#82FE1A'},
+    sixties: {source:"196", color: '#32AE0E'},
+    seventies: {source:"197", color: '#1AFE89'},
+    eighties: {source:"198", color: '#1AFEFB'},
+    nineties: {source:"199", color: '#1A63FE'},
+    aughts: {source:"200", color: '#891AFE'},
+    ohtens: {source:"201", color: '#FE1AFB'},
+    ohtwenties: {source:"202", color: '#FE1A96'}
+};
+
 var geoJsonLayers = {}; //to put the geojson into
-// add geojson to the map
-var airphotovector = L.geoJSON(airphotopoly, {   
-  style: function(feature) {
-    return {color: 'red'};  //colour the polygons
-  },
-  //on each feature, add a popup with the photo number and date
-  onEachFeature: function (feature, info) { 
-      info.bindPopup('<p>Photo ID: '+feature.properties.PHOTO_ID+'</p>'+'<p>Photo Date: '+feature.properties.Photo_Date+'</p>')
-  }
-}).addTo(map);
+
+for(var layer in layerInfo) {
+    geoJsonLayers[layer] = L.geoJSON(airphotopoly, {
+        filter: function(feature) {
+            if(feature.properties.date[layerInfo[layer].source] == feature.properties.date[layerInfo[layer]].slice(0,3)) {
+                return feature;
+            }
+        },
+        style: function(feature) {
+            return {
+                color: layerInfo[layer].color
+            }
+        },
+        onEachFeature: function (feature, info) { 
+            info.bindPopup('<p>Photo ID: '+feature.properties.PHOTO_ID+'</p>'+'<p>Photo Date: '+feature.properties.Photo_Date+'</p>')
+        }
+    }).addTo(map);
+};
+
+var sourcesLabels = {
+    "1920's": geoJsonLayers.photoLayer
+};
 
 // add layer control for sorting through the geojson
-const photoControl = L.control.layers(null,airphotovector,{collapsed:false}).addTo(map);
+var thingy = L.control.layers(null, sourcesLabels, {collapsed:false,position:'topright'}).addTo(map);
 // this control needs to:
 //  - show items from the geojson
 //  - restrict based on what is visible
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// this section attempts to filter by map bounds
+

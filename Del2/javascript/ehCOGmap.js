712908c322a7d2d26f73011b9d1bca3a0c5254dd
edmharris <ewan.harris@flemingcollege.ca>
2023-05-31 18:12:37 -0400

    New code

--- Del2/javascript/ehCOGmap.js
+++ Del2/javascript/ehCOGmap.js
@@ -1,112 +1,31 @@
-// Add map baselayers
-const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
-const Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-});
-//Map declaration
-const map = L.map('map', {
-    center: [44.3, -77.8],    //44.5, -78
-    zoom: 12,
-    layers: [osm, Esri_WorldImagery]            
-});
-// Add baselayer info as array for layer control, control added lower to put it
-// below the search bar
-const baseLayers = {
-    'Esri World Imagery': Esri_WorldImagery,
-    'OpenStreetMap': osm
-};
+// Create a Leaflet map
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-// add geosearch control
-var geocoder = L.Control.geocoder({
-    collapsed: false,       // keep it large
-    position: 'topright',   // put it in the upper right corner
-    defaultMarkGeocode: false
-}).on('markgeocode', function(result) {
-    const coords = [result.geocode.center.lat, result.geocode.center.lng]; 
-    var searchMarker = L.marker(coords, {
-        draggable: true //create draggable marker
-    }).addTo(map);
-    map.setView(coords,17); // move the map view to the searched location
-})
-.addTo(map);
-
-//Add layer control button to switch between imagery and openstreetmap
-const baseControl = L.control.layers(baseLayers,null,{position:'topleft'}).addTo(map);
-
-/////////////////////////////////////////////////////////////////////////////////////////
-// This section attempts to add a control to filter by decade
-
-var layerInfo = {
-    twenties: {source:"192", color: '#FE1A1A'},
-    thirties: {source:"193", color: '#FEAB1A'},
-    fourties: {source:"194", color: '#FEFE1A'},
-    fifties: {source:"195", color: '#82FE1A'},
-    sixties: {source:"196", color: '#32AE0E'},
-    seventies: {source:"197", color: '#1AFE89'},
-    eighties: {source:"198", color: '#1AFEFB'},
-    nineties: {source:"199", color: '#1A63FE'},
-    aughts: {source:"200", color: '#891AFE'},
-    ohtens: {source:"201", color: '#FE1AFB'},
-    ohtwenties: {source:"202", color: '#FE1A96'}
-};
-
-var geoJsonLayers = {}; //to put the geojson into
-
-for(var layer in layerInfo) {
-    geoJsonLayers[layer] = L.geoJSON(airphotopoly, {
-        filter: function(feature) {
-            if(feature.properties.date[layerInfo[layer].source] == feature.properties.date.slice(0,2)) {
-                return feature;
-            }
-        },
-        style: function(feature) {
-            return {
-                color: layerInfo[layer].color
-            }
-        },
-        onEachFeature: function (feature, info) { 
-            info.bindPopup('<p>Photo ID: '+feature.properties.PHOTO_ID+'</p>'+'<p>Photo Date: '+feature.properties.Photo_Date+'</p>')
-        }
-    }).addTo(map);
-};
-
-var sourcesLabels = {
-    "1920's": geoJsonLayers.twenties,
-    "1930's": geoJsonLayers.thirties,
-    "1940's": geoJsonLayers.fourties,
-    "1950's": geoJsonLayers.fifties,
-    "1960's": geoJsonLayers.sixties,
-    "1970's": geoJsonLayers.seventies,
-    "1980's": geoJsonLayers.eighties,
-    "1990's": geoJsonLayers.nineties,
-    "2000's": geoJsonLayers.aughts,
-    "2010's": geoJsonLayers.ohtens,
-    "2020's": geoJsonLayers.ohtwenties
-};
-
-// add layer control for sorting through the geojson
-var imageControl = L.control.layers(null,sourcesLabels, {collapsed:false,position:'topright'}).addTo(map);
-// // this control needs to:
-// //  - show items from the geojson
-// //  - restrict based on what is visible
-
-/////////////////////////////////////////////////////////////////////////////////////////
-// this section attempts to filter by map bounds
-
-var mapBounds = map.getBounds();
-console.log("Just testing.",mapBounds);
-
-map.on('move',function() {
-    mapBounds = map.getBounds()
-});
-
-L.geoJSON(airphotopoly, {
-    style: function(feature) {
-        return {color: 'yellow'}
-    }
-}).bindPopup(function (layer) {
-    return layer.feature.properties.date.slice(0,3);
+// Add a base layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 18,
 }).addTo(map);
+
+// Load the GeoTIFF file
+var url = 'https://ssfcollege.sharepoint.com/:i:/r/sites/CollabTeam2306/Shared%20Documents/General/images/A16868-154_COG.TIF?csf=1&web=1&e=sV5xaU';
+fetch(url)
+    .then(function(response) {
+        return response.arrayBuffer();
+    })
+    .then(function(arrayBuffer) {
+        // Create a GeoTIFF object from the array buffer
+        return GeoTIFF.fromArrayBuffer(arrayBuffer);
+    })
+    .then(function(geotiff) {
+        // Read the first image from the GeoTIFF
+        return geotiff.getImage();
+    })
+    .then(function(image) {
+        // Create a Leaflet GeoTIFF layer from the image
+        var tiffLayer = L.leafletGeotiff(image).addTo(map);
+        map.fitBounds(tiffLayer.getBounds());
+    })
+    .catch(function(error) {
+        console.log('Error:', error);
+    });

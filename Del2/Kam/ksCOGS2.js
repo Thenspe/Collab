56026a3539933cb25228ffbc85c1d3ecc4c4d9f8
KamrynK <kamryn.sandiford@flemingcollege.ca>
2023-05-31 16:37:52 -0400

    original

--- Del2/Kam/ksCOGS2.js
+++ Del2/Kam/ksCOGS2.js
@@ -21,37 +21,6 @@ const Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/re
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
-// Add baselayer info as array for layer control, control added lower to put it
-// below the search bar
-const baseLayers = {
-  'OpenStreetMap': osm,
-  'Esri World Imagery': Esri_WorldImagery
-};
-
-// // add geosearch control
-// var geocoder = L.Control.geocoder({
-//   collapsed: false,
-//   position: 'topright',
-//   defaultMarkGeocode: false
-// }).on('markgeocode', function(result) {
-//   const coords = [result.geocode.center.lat, result.geocode.center.lng];
-//   var searchMarker = L.marker(coords, {
-//       draggable: true //create draggable marker
-//   }).addTo(map);
-//   map.setView(coords,17);
-// })
-// .addTo(map);
-
-//Add layer control button to switch between imagery and openstreetmap
-const layerControl = L.control.layers(baseLayers).addTo(map);
-
-// add air photo database geojson
-var airphotos = L.geoJSON(photos, {
-  onEachFeature: function (feature, info) {
-      info.bindPopup('<p>Photo ID: '+feature.properties.PHOTO_ID+'</p>')
-  }
-}).addTo(map);
-
 const url_to_geotiff_file = new URLSearchParams(location.search).get("url");
 console.log("url_to_geotiff_file:", url_to_geotiff_file);
 

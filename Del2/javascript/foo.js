3e01dd84b8f5d5b770b21db34300ca4a67f797a7
edmharris <ewan.harris@flemingcollege.ca>
2023-05-31 18:48:14 -0400

    web server

---
+++ Del2/javascript/foo.js
@@ -0,0 +1,31 @@
+// Create a Leaflet map
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+// Add a base layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+}).addTo(map);
+
+// Load the GeoTIFF file
+var url = 'https://ssfcollege.sharepoint.com/:i:/r/sites/CollabTeam2306/Shared%20Documents/General/images/A16868-154_COG.TIF?csf=1&web=1&e=sV5xaU';
+fetch(url)
+    .then(function(response) {
+        return response.arrayBuffer();
+    })
+    .then(function(arrayBuffer) {
+        // Create a GeoTIFF object from the array buffer
+        return GeoTIFF.fromArrayBuffer(arrayBuffer);
+    })
+    .then(function(geotiff) {
+        // Read the first image from the GeoTIFF
+        return geotiff.getImage();
+    })
+    .then(function(image) {
+        // Create a Leaflet GeoTIFF layer from the image
+        var tiffLayer = L.leafletGeotiff(image).addTo(map);
+        map.fitBounds(tiffLayer.getBounds());
+    })
+    .catch(function(error) {
+        console.log('Error:', error);
+    });

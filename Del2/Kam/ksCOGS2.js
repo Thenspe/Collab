82c29f6884bd18b0103de8bd75f58652bfb0307a
KamrynK <kamryn.sandiford@flemingcollege.ca>
2023-05-31 16:41:02 -0400

    example file

--- Del2/Kam/ksCOGS2.js
+++ Del2/Kam/ksCOGS2.js
@@ -28,8 +28,7 @@ if (!url_to_geotiff_file) {
   setTimeout(function() {
     // if didn't pass in a url, redirect to planet example
     const parser = new URL(window.location);
-    parser.searchParams.set("url", "https://ssfcollege-my.sharepoint.com/personal/ksandifo_flemingcollege_ca/_layouts/15/onedrive.aspx?listurl=https%3A%2F%2Fssfcollege%2Esharepoint%2Ecom%2Fsites%2FCollabTeam2306%2FShared%20Documents&viewid=5ce75b83%2D0ef9%2D4cfb%2D8f02%2D3e9b1d0bfcf5&login_hint=ksandifo%40flemingcollege%2Eca&id=%2Fsites%2FCollabTeam2306%2FShared%20Documents%2FGeneral%2Fimages%2FA16868%2D154%5FCOG%2ETIF&parent=%2Fsites%2FCollabTeam2306%2FShared%20Documents%2FGeneral%2Fimages");
-    // "https://storage.googleapis.com/pdd-stac/disasters/hurricane-harvey/0831/20170831_172754_101c_3b_Visual.tif");
+    parser.searchParams.set("url", "https://storage.googleapis.com/pdd-stac/disasters/hurricane-harvey/0831/20170831_172754_101c_3b_Visual.tif");
     window.location = parser.href;
   }, 2 * 1000);
 } else {

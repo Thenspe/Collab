4516975adb41018c9f381bb272c974311dd4f754
edmharris <ewan.harris@flemingcollege.ca>
2023-06-02 15:10:52 -0400

    COG map load

--- Del2/javascript/ehCOGmap.js
+++ Del2/javascript/ehCOGmap.js
@@ -8,7 +8,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Load the GeoTIFF file
-var url = 'C:\Courses\GEOM68\Sandbox\GEOM68_WellWaterDB\P2306\Del2\imagery\TrentUniFreeTIFs';
+var url = 'Del2\imagery\TrentUniFreeTIFs\A02033-009_cog.tif';
 fetch(url)
     .then(function(response) {
         return response.arrayBuffer();
